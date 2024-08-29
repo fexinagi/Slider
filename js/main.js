1028989f15c13784699c988220cb01cbe4133c43
Fexi Nagi <fexinagi@Fexi-MacBook-Pro.local>
2024-08-29 19:49:55 +0400

    Slider

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var slides = document.querySelectorAll(".slide")
+var nextBtn = document.querySelector(".btn-next")
+var prevBtn = document.querySelector(".btn-prev")
+
+var currentIndex = 0;
+var maxNumber = slides.length - 1;
+
+
+// nextBtn.addEventListener("click", function () {
+//     if (currentIndex === maxNumber) {
+//         currentIndex = 0
+//     } else {
+//         currentIndex ++
+//     }
+    
+//     slides.forEach((slide, index) => {
+//       slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+//     });
+// })
+
+
+NextButtonClick()
+function NextButtonClick() {
+    if (currentIndex === maxNumber) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+
+    slides.forEach((slide, index) => {
+        
+        slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+    });
+}
+nextBtn.addEventListener("click", NextButtonClick);
+
+
+
+// prevBtn.addEventListener("click", () => {
+//     if (currentIndex === 0) {
+//         currentIndex = maxNumber
+//     } else {
+//         currentIndex --
+//     }
+
+//     slides.forEach((slide, index) => {
+//       slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+//     });
+// })
+
+
+
+function PrevButtonClick() {
+    if (currentIndex === 0) {
+        currentIndex = maxNumber;
+    } else {
+        currentIndex--;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+    });
+}
+prevBtn.addEventListener("click", PrevButtonClick)
